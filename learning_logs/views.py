81cc07eb8498d7cbdf39cf9bc3f8188b67e35ed5
mateusz <gmateusz92@gmail.com>
2022-10-12 22:06:11 +0200

    1

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,28 +1,61 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
 from .forms import TopicForm, EntryForm
-from .models import Topic
+from .models import Topic, Entry
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 
 def index(request):
-
      return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
-
-      topics = Topic.objects.order_by('date_added')
+      topics = Topic.objects.filter(owner=request.user).order_by('date_added')
       context = {'topics': topics}
       return render(request, 'learning_logs/topics.html', context)
 
+# def edit_entry(request, entry_id):
+#     entry = Entry.objects.get(id=entry_id)
+#     topic = entry.topic
+#     form = EntryForm(request.POST, instance=entry)
+#     if request.method == 'POST':
+#
+#         if form.is_valid():
+#             form.save()
+#             return redirect('topics', topic_id=topic.id)
+#             #return HttpResponseRedirect(reverse('topics'))
+#     context = {'form': form, 'entry': entry, 'topic': topic}
+#     return render(request, 'learning_logs/new_topic.html', context)
+
+def edit_entry(request, entry_id):
+     """Edycja istniejącego wpisu."""
+     entry = Entry.objects.get(id=entry_id)
+     topic = entry.topic
+     if request.method != 'POST':
+         # Żądanie początkowe, wypełnienie formularza aktualną treścią wpisu.
+         form = EntryForm(instance=entry)
+     else:
+         # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
+         form = EntryForm(instance=entry, data=request.POST)
+         if form.is_valid():
+            form.save()
+            return redirect('topic', topic_id=topic.id)
+     context = {'entry': entry, 'topic': topic, 'form': form}
+     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+
+@login_required
 def topic(request, topic_id):
-
      topic = Topic.objects.get(id=topic_id)
-
-     entries = topic.entry_set.order_by('date_added')
+     #upewniamy sie ze temat nalezy do biezacego uzytkownika
+     if topic.owner != request.user:
+        raise Http404
+     entries = topic.entry_set.order_by('date_added') #entry_set tworzymy na podstawie foreignkey
      context = {'topic': topic, 'entries': entries}
      return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         # Nie przekazano żadnych danych, należy utworzyć pusty formularz.
@@ -31,24 +64,44 @@ def new_topic(request):
         # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('topics'))
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return redirect('topics')
+            #return HttpResponseRedirect(reverse('topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
+
+# def new_entry(request, topic_id):
+#     """Dodanie nowego wpisu dla określonego tematu."""
+#     topic = Topic.objects.get(id=topic_id)
+#     if request.method != 'POST':
+#         # Nie przekazano żadnych danych, należy utworzyć pusty formularz.
+#         form = EntryForm()
+#     else:
+#         # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
+#         form = EntryForm(request.POST)
+#         if form.is_valid():
+#             new_entry = form.save(commit=False)
+#             new_entry.topic = topic
+#             new_entry.save()
+#             return redirect('topic', topic_id=topic_id)
+#     context = {'form': form, 'topic': topic}
+#     return render(request, 'learning_logs/new_entry.html', context)
+
+@login_required
 def new_entry(request, topic_id):
     """Dodanie nowego wpisu dla określonego tematu."""
     topic = Topic.objects.get(id=topic_id)
-    if request.method != 'POST':
-        # Nie przekazano żadnych danych, należy utworzyć pusty formularz.
-        form = EntryForm()
-    else:
-        # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
+    form = EntryForm()
+    if request.method == 'POST':
         form = EntryForm(request.POST)
         if form.is_valid():
-            new_entry = form.save(commit=False)
-            new_entry.topic = topic
+            new_entry = form.save(commit=False) # a metody save() dołączamy argument commit=False (patrz wiersz ), aby nakazać Django utworzenie nowego obiektu wpisu i jego przechowywanie w zmiennej new_entry, ale jeszcze bez zapisywania w bazie danych
+            new_entry.topic = topic # Atrybutowi new_entry egzemplarza topic przypisujemy temat pobrany z bazydanych na początku kodu funkcji (patrz wiersz ), a następnie wywołujemysave() bez argumentów. W ten sposób wpis zostanie zapisany w bazie danychwraz z przypisanym mu prawidłowym tematem.
             new_entry.save()
-            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
-    context = {'form': form}
+            return redirect('topic', topic_id=topic_id)
+    context = {'form': form, 'topic': topic}
     return render(request, 'learning_logs/new_entry.html', context)
