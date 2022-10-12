b1d0c086e70a17758825a00f307d9a57b0519e3a
mateusz <gmateusz92@gmail.com>
2022-10-12 22:06:11 +0200

    1

--- learning/settings.py
+++ learning/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_logs'
+    'learning_logs',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/user/login'
